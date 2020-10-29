394a56d854b43fc012b641a26386d5ee451bcc5f
Mamé <mameazad.turgut@gmail.com>
2020-10-29 15:43:39 +0100

    made post method

--- data/home.md
+++ data/home.md
@@ -1,10 +1,3 @@
-# Welcome!
+hellosssss mame ddd
 
-This is your own wik
-This ishow you change stuff!!
-
-Here's the [about](about) page.
-
-To create a new file, just link to it and follow the link: [badging](badging)
-
-#home #default
+#mame #hello #home
--- server.js
+++ server.js
@@ -2,28 +2,30 @@
 
 // require built-in dependencies
 const path = require('path');
-const util = require('util');
+
+const util = require('util'); // The util module supports the needs of Node.js internal APIs. Many of the utilities are useful for application and module developers as well
 const fs = require('fs');
 
-const readFile = util.promisify(fs.readFile);
+const readFile = util.promisify(fs.readFile); //equivalently using async functions
 const writeFile = util.promisify(fs.writeFile);
 const readDir = util.promisify(fs.readdir);
 
 // require express-related dependencies
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors'); //Cross Origin Resource Sharing,  CORS essentially means cross-domain requests.
 const bodyParser = require('body-parser');
 
 // require local dependencies
 const logger = require('./middleware/logger');
 
 // declare local constants and helper functions
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
+
 const DATA_DIR = 'data';
 const TAG_RE = /#\w+/g;
 const slugToPath = (slug) => {
   const filename = `${slug}.md`;
-  return path.join(DATA_DIR, filename);
+  return path.join(DATA_DIR, filename); //Returns: '/data/x.md => https://nodejs.org/api/path.html#path_path_join_paths
 };
 
 // initialize express app
@@ -40,14 +42,17 @@ app.use(bodyParser.json());
 // $ yarn build
 app.use('/', express.static(path.join(__dirname, 'client', 'build')));
 
-
-
 // GET: '/api/page/:slug'
 // success response: {status: 'ok', body: '<file contents>'}
 // failure response: {status: 'error', message: 'Page does not exist.'}
 app.get('/api/page/:slug', async (req, res) => {
   const filename = slugToPath(req.params.slug);
   try {
+    // before write a file read it
+
+    const file = await readFile(filename, 'utf8');
+    // console.log('File start to read', file);
+    //await fs.writeFile('filename.txt', 'test');
     const body = await readFile(filename, 'utf-8');
     res.json({ status: 'ok', body });
     // return jsonOK(res, { body });
@@ -57,7 +62,6 @@ app.get('/api/page/:slug', async (req, res) => {
   }
 });
 
-
 // POST: '/api/page/:slug'
 //  body: {body: '<file text content>'}
 // tries to write the body to the given file
@@ -66,22 +70,25 @@ app.get('/api/page/:slug', async (req, res) => {
 app.post('/api/page/:slug', async (req, res) => {
   const filename = slugToPath(req.params.slug);
   try {
+    // read request body which still is not file
+    const text = req.body;
+    // write current change to in file
+    await writeFile(filename, text.body);
 
+    console.log('Files has been changed', text);
+    res.json({ status: 'ok', text });
+    //res.json({ status: 'ok', text });
   } catch (e) {
-
+    res.json({ status: 'error', message: 'Could not write page' });
   }
 });
-
 
 // GET: '/api/pages/all'
 // sends an array of all file names in the DATA_DIR
 // file names do not have .md, just the name!
 //  success response: {status:'ok', pages: ['fileName', 'otherFileName']}
 //  failure response: no failure response
-app.get('/api/pages/all', async (req, res) => {
-
-});
-
+app.get('/api/pages/all', async (req, res) => {});
 
 // GET: '/api/tags/all'
 // sends an array of all tag names in all files, without duplicates!
@@ -89,20 +96,14 @@ app.get('/api/pages/all', async (req, res) => {
 // hint: use the TAG_RE regular expression to search the contents of each file
 //  success response: {status:'ok', tags: ['tagName', 'otherTagName']}
 //  failure response: no failure response
-app.get('/api/tags/all', async (req, res) => {
-
-});
-
+app.get('/api/tags/all', async (req, res) => {});
 
 // GET: '/api/tags/:tag'
 // searches through the contents of each file looking for the :tag
 // it will send an array of all file names that contain this tag (without .md!)
 //  success response: {status:'ok', tag: 'tagName', pages: ['tagName', 'otherTagName']}
 //  failure response: no failure response
-app.get('/api/tags/:tag', async (req, res) => {
-
-});
-
+app.get('/api/tags/:tag', async (req, res) => {});
 
 // this needs to be here for the frontend to create new wiki pages
 //  if the route is not one from above
@@ -111,11 +112,10 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
-
 app.listen(PORT, (err) => {
   if (err) {
     console.error(err);
     return;
   }
-  console.log(`Wiki app is serving at http://localhost:${PORT}`)
+  console.log(`Wiki app is serving at http://localhost:${PORT}`);
 });
